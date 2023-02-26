e6a8d6e14853f1e215e84ffdec1b016842b6dd86
mdupree <mikerdupree@gmail.com>
2023-02-26 01:04:17 -0800

    Lualine, explorer, lsp updates

--- lua/explorer.lua
+++ lua/explorer.lua
@@ -8,85 +8,88 @@ vim.opt.termguicolors = true
 
 -- OR setup with some options
 require("nvim-tree").setup({
-    sort_by = "case_sensitive",
-    git = {
-        enable = true,
-        ignore = false
+  sort_by = "case_sensitive",
+  git = {
+    enable = true,
+    ignore = false
+  },
+  view = {
+    width = 20,
+    --number = true,
+    --relativenumber = true,
+    adaptive_size = true,
+    mappings = {
+      list = {
+        --                { key = "u", action = "dir_up" },
+      },
     },
-    view = {
-        width = 25,
-        adaptive_size = true,
-        mappings = {
-            list = {
---                { key = "u", action = "dir_up" },
-            },
+  },
+  renderer = {
+    group_empty = true,
+    indent_markers = {
+      enable = true,
+      inline_arrows = true,
+      icons = {
+        corner = "└",
+        edge = "",
+        item = "|",
+        bottom = "─",
+        none = " ",
+      },
+    },
+    icons = {
+      webdev_colors = true,
+      git_placement = "after",
+      padding = " ",
+      symlink_arrow = " ➛ ",
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
+        git = true,
+      },
+      glyphs = {
+        default = "",
+        symlink = "",
+        bookmark = "",
+        folder = {
+          arrow_closed = "└",
+          edge = "",
+          arrow_open = "└",
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = "",
         },
-    },
-    renderer = {
-        group_empty = true,
-        indent_markers = {
-            enable = true,
-            inline_arrows = true,
-            icons = {
-                corner = "└",
-                edge = "",
-                item = "|",
-                bottom = "─",
-                none = " ",
-            },
+        git = {
+          unstaged = "☐",
+          staged = "☑",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "",
+          deleted = "",
+          ignored = "◌",
         },
-        icons = {
-            webdev_colors = true,
-            git_placement = "after",
-            padding = " ",
-            symlink_arrow = " ➛ ",
-            show = {
-                file = true,
-                folder = true,
-                folder_arrow = true,
-                git = true,
-            },
-            glyphs = {
-                default = "",
-                symlink = "",
-                bookmark = "",
-                folder = {
-                    arrow_closed = "⊧",
-                    arrow_open = "⊦",
-                    default = "",
-                    open = "",
-                    empty = "",
-                    empty_open = "",
-                    symlink = "",
-                    symlink_open = "",
-                },
-                git = {
-                    unstaged = "✗",
-                    staged = "",
-                    unmerged = "",
-                    renamed = "➜",
-                    untracked = "",
-                    deleted = "",
-                    ignored = "◌",
-                },
-            },
-        },
+      },
     },
-    diagnostics = {
-        enable = true,
-        show_on_dirs = false,
-        debounce_delay = 50,
-        icons = {
-            hint = "",
-            info = "",
-            warning = "",
-            error = "ﱥ",
-        },
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = false,
+    debounce_delay = 50,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "⚑",
     },
-    filters = {
-        dotfiles = false,
-    },
-    actions = {
-        use_system_clipboard = true,
-    }
+  },
+  filters = {
+    dotfiles = false,
+  },
+  actions = {
+    use_system_clipboard = true,
+  }
 })
--- lua/lua_line.lua
+++ lua/lua_line.lua
@@ -1,47 +1,251 @@
+
+local lualine = require('lualine')
 -- GPS for lua line
 require("nvim-gps").setup()
 local gps = require("nvim-gps")
+-- Color table for highlights
+-- stylua: ignore
+local colors = {
+  bg       = '#000000',
+  fg       = '#bbc2cf',
+  yellow   = '#ECBE7B',
+  cyan     = '#008080',
+  darkblue = '#081633',
+  green    = '#98be65',
+  orange   = '#FF8800',
+  violet   = '#a9a1e1',
+  magenta  = '#c678dd',
+  blue     = '#51afef',
+  red      = '#ec5f67',
+}
 
--- Configure lua line
-require('lualine').setup {
+local conditions = {
+  buffer_not_empty = function()
+    return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
+  end,
+  hide_in_width = function()
+    return vim.fn.winwidth(0) > 80
+  end,
+  check_git_workspace = function()
+    local filepath = vim.fn.expand('%:p:h')
+    local gitdir = vim.fn.finddir('.git', filepath .. ';')
+    return gitdir and #gitdir > 0 and #gitdir < #filepath
+  end,
+}
+
+
+-- Config
+local config = {
   options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '祈', right = '祈' },
-    section_separators = { left = '', right = '' },
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
+    -- Disable sections and component separators
+    component_separators = '',
+    section_separators = '',
+    theme = {
+      -- We are going to use lualine_c an lualine_x as left and
+      -- right section. Both are highlighted by c theme .  So we
+      -- are just setting default looks o statusline
+      normal = { c = { fg = colors.fg, bg = colors.bg } },
+      inactive = { c = { fg = colors.fg, bg = colors.bg } },
     },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
   },
   sections = {
-    lualine_a = {
-      { 'mode', fmt = function(str) return str:sub(1, 1) end }
-    },
-    lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = { { gps.get_location, cond = gps.is_available } },
-    lualine_x = { { 'filename', icon = { 'פּ', color = { fg = 'green' } } } },
-    lualine_y = { 'progress' },
-    lualine_z = { 'os.date()' }
-  },
-  inactive_sections = {
+    -- these are to remove the defaults
     lualine_a = {},
     lualine_b = {},
-    lualine_c = { 'filename' },
-    lualine_x = { 'location' },
     lualine_y = {},
-    lualine_z = {}
+    lualine_z = {},
+    -- These will be filled later
+    lualine_c = {},
+    lualine_x = {},
   },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
+  inactive_sections = {
+    -- these are to remove the defaults
+    lualine_a = {},
+    lualine_b = {},
+    lualine_y = {},
+    lualine_z = {},
+    lualine_c = {},
+    lualine_x = {},
+  },
 }
+
+-- Inserts a component in lualine_c at left section
+local function ins_left(component)
+  table.insert(config.sections.lualine_c, component)
+end
+
+-- Inserts a component in lualine_x ot right section
+local function ins_right(component)
+  table.insert(config.sections.lualine_x, component)
+end
+-- section_separators = { left = '', right = '' },
+ins_left {
+  function()
+    return ''
+  end,
+  color = { fg = colors.magenta }, -- Sets highlighting of component
+  padding = { left = 0, right = 1 }, -- We don't need space before this
+}
+
+ins_left {
+  -- mode component
+  function()
+    local mode_symbol = {
+      n = '🔰',
+      i = '⚒',
+      v = 'v',
+      [''] = '^V',
+      V = 'V',
+      c = '⚡',
+      no = 'no',
+      s = 's',
+      S = 'S',
+      [''] = '^S',
+      ic = 'ic',
+      R = 'R✛',
+      Rv = 'Rv',
+      cv = 'cv',
+      ce = 'ce',
+      r = 'r',
+      rm = 'rm',
+      ['r?'] = 'r?',
+      ['!'] = '!',
+      t = 't'
+    }
+    return mode_symbol[vim.fn.mode()]
+    --return ':MODE'
+  end,
+  color = function()
+    -- auto change color according to neovims mode
+    local mode_color = {
+      n = colors.red,
+      i = colors.green,
+      v = colors.blue,
+      [''] = colors.blue,
+      V = colors.blue,
+      c = colors.magenta,
+      no = colors.red,
+      s = colors.orange,
+      S = colors.orange,
+      [''] = colors.orange,
+      ic = colors.yellow,
+      R = colors.violet,
+      Rv = colors.violet,
+      cv = colors.red,
+      ce = colors.red,
+      r = colors.cyan,
+      rm = colors.cyan,
+      ['r?'] = colors.cyan,
+      ['!'] = colors.red,
+      t = colors.red,
+    }
+    return { fg = mode_color[vim.fn.mode()] }
+  end,
+  padding = { right = 1 },
+}
+
+ins_left {
+  -- filesize component
+  'filesize',
+  cond = conditions.buffer_not_empty,
+}
+
+ins_left {
+  'path',
+  cond = conditions.buffer_not_empty,
+  color = { fg = '#ffffff', gui = 'bold' },
+}
+ins_left {
+  'filename',
+  cond = conditions.buffer_not_empty,
+  color = { fg = '#ffffff', gui = 'bold' },
+}
+
+ins_left { 'location' }
+
+ins_left { 'progress', color = { fg = colors.fg, gui = 'bold' } }
+
+ins_left {
+  'diagnostics',
+  sources = { 'nvim_diagnostic' },
+  symbols = { error = ' ', warn = ' ', info = ' ' },
+  diagnostics_color = {
+    color_error = { fg = colors.red },
+    color_warn = { fg = colors.yellow },
+    color_info = { fg = colors.cyan },
+  },
+}
+
+-- Insert mid section. You can make any number of sections in neovim :)
+-- for lualine it's any number greater then 2
+ins_left {
+  function()
+    return '%='
+  end,
+}
+
+ins_left {
+  -- Lsp server name .
+  function()
+    local msg = 'No Active Lsp'
+    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+    local clients = vim.lsp.get_active_clients()
+    if next(clients) == nil then
+      return msg
+    end
+    for _, client in ipairs(clients) do
+      local filetypes = client.config.filetypes
+      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+        return client.name
+      end
+    end
+    return msg
+  end,
+  icon = '',
+  color = { fg = colors.fg, gui = 'bold' },
+}
+
+-----------------------------------
+-- Add components to right sections
+----------------------------------
+ins_right {
+  'o:encoding', -- option component same as &encoding in viml
+  fmt = string.upper,
+  cond = conditions.hide_in_width,
+  color = { fg = colors.green, gui = 'bold' },
+}
+
+ins_right {
+  'fileformat',
+  fmt = string.upper,
+  icons_enabled = true,
+  color = { fg = colors.cyan, gui = 'bold' },
+}
+
+ins_right {
+  'branch',
+  icon = '',
+  color = { fg = colors.violet, gui = 'bold' },
+}
+
+ins_right {
+  'diff',
+  symbols = { added = ' ', modified = '柳', removed = ' ' },
+  diff_color = {
+    added = { fg = colors.green },
+    modified = { fg = colors.orange },
+    removed = { fg = colors.red },
+  },
+  cond = conditions.hide_in_width,
+}
+
+ins_right {
+  function()
+    return ''
+  end,
+  color = { fg = colors.magenta },
+  padding = { left = 1 },
+}
+
+lualine.setup(config)
+
