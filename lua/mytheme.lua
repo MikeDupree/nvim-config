77743175754946a93140361d685f01ded3cac45c
MikeDupree <mikerdupree@gmail.com>
2023-03-22 08:33:14 -0700

    wip lazy vim

--- lua/mytheme.lua
+++ lua/mytheme.lua
@@ -3,7 +3,7 @@
 --
 vim.opt.termguicolors = true
 --vim.g.sonokai_transparent_background = 2
-vim.cmd "colorscheme dupine"
+vim.cmd "colorscheme rose-pine"
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -2,5 +2,5 @@ return {
   { "nvim-telescope/telescope.nvim", version = '0.1.0' },
   'ThePrimeagen/harpoon',
   'mbbill/undotree',
-
+  'terrortylor/nvim-comment'
 }
