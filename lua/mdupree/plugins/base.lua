b6ff5dfcd3739d70ca49dbc9ddb8f94c0ce60aca
mdupree <mikerdupree@gmail.com>
2023-03-27 20:42:27 -0700

    switch to lsp-zero, move plugin configs to plugin import config section

--- lua/mdupree/plugins/base.lua
+++ lua/mdupree/plugins/base.lua
@@ -1,6 +1,11 @@
 return {
 	"nvim-tree/nvim-web-devicons",
 	"nvim-lualine/lualine.nvim",
-	"ggandor/leap.nvim",
+	{
+		"ggandor/leap.nvim",
+		config = function()
+			require("leap").add_default_mappings()
+		end,
+	},
 	"mbbill/undotree",
 }
--- lua/mdupree/plugins/lsp.lua
+++ lua/mdupree/plugins/lsp.lua
@@ -1,16 +1,84 @@
 return {
 	{
-		"neovim/nvim-lspconfig",
+		"VonHeikemen/lsp-zero.nvim",
+		version = "v1.x",
 		dependencies = {
-			-- auto install LSPs
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			-- Status updates for LSP
-			"j-hui/fidget.nvim",
-			"lvimuser/lsp-inlayhints.nvim",
-			"nvim-lua/plenary.nvim",
-			"folke/neodev.nvim",
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{ "williamboman/mason.nvim" }, -- Optional
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
+
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "hrsh7th/cmp-buffer" }, -- Optional
+			{ "hrsh7th/cmp-path" }, -- Optional
+			{ "saadparwaiz1/cmp_luasnip" }, -- Optional
+			{ "hrsh7th/cmp-nvim-lua" }, -- Optional
+
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" }, -- Required
+			{ "rafamadriz/friendly-snippets" }, -- Optional
 		},
+		config = function()
+			local lsp = require("lsp-zero")
+
+			lsp.preset("recommended")
+
+			lsp.ensure_installed({
+				"tsserver",
+				"eslint",
+				"rust_analyzer",
+				"intelephense",
+			})
+
+			local cmp = require("cmp")
+			local cmp_select = { behavior = cmp.SelectBehavior.Select }
+			local cmp_mappings = lsp.defaults.cmp_mappings({
+				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				["<C-Space>"] = cmp.mapping.complete(),
+			})
+
+			lsp.set_preferences({
+				--sign_icons = { }
+			})
+
+			lsp.setup_nvim_cmp({
+				mapping = cmp_mappings,
+			})
+
+			lsp.on_attach(function(client, bufnr)
+				local opts = { buffer = bufnr, remap = false }
+
+				vim.keymap.set("n", "gd", function()
+					vim.lsp.buf.definition()
+				end, opts)
+				vim.keymap.set("n", "K", function()
+					vim.lsp.buf.hover()
+				end, opts)
+				vim.keymap.set("n", "<leader>vws", function()
+					vim.lsp.buf.workspace_symbol()
+				end, opts)
+				vim.keymap.set("n", "<leader>vd", function()
+					vim.diagnostic.open_float()
+				end, opts)
+				vim.keymap.set("n", "[d", function()
+					vim.diagnostic.goto_next()
+				end, opts)
+				vim.keymap.set("n", "]d", function()
+					vim.diagnostic.goto_prev()
+				end, opts)
+				vim.keymap.set("n", "<leader>.", function()
+					vim.lsp.buf.code_actions()
+				end, opts)
+				vim.keymap.set("i", "<C-h>", function()
+					vim.lsp.buf.signature_help()
+				end, opts)
+			end)
+
+			lsp.setup()
+		end,
 	},
-	"SmiteshP/nvim-navic",
 }
