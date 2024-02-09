e79dee0444b9c10f151982fcdd733ff03c97f8ae
mdupree <mikerdupree@gmail.com>
2024-02-09 08:55:00 -0800

    Get Navic working properly

--- lua/mdupree/plugins/colorschemes.lua
+++ lua/mdupree/plugins/colorschemes.lua
@@ -1,14 +1,35 @@
 return {
-  {
-    "sainnhe/everforest",
-    config = function()
-      local colorscheme = "everforest"
-      local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-      if not status_ok then
-        vim.notify("colorscheme " .. colorscheme .. " not installed!")
-        return
-      end
-      vim.opt.termguicolors = true
-    end,
-  },
+	{
+		"sainnhe/everforest",
+		config = function()
+			--local colorscheme = "everforest"
+			--local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+			--if not status_ok then
+			--	vim.notify("colorscheme " .. colorscheme .. " not installed!")
+			--return
+			--end
+			--vim.opt.termguicolors = true
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		config = function()
+			local colorscheme = "tokyonight-night"
+			local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+			if not status_ok then
+				vim.notify("colorscheme " .. colorscheme .. " not installed!")
+				return
+			end
+			vim.opt.termguicolors = true
+
+			require("tokyonight").setup({
+				style = "storm",
+				transparent = true,
+				terminal_colors = true,
+			})
+		end,
+	},
 }
--- lua/mdupree/plugins/lsp.lua
+++ lua/mdupree/plugins/lsp.lua
@@ -5,7 +5,7 @@ return {
     dependencies = {
       -- LSP Support
       { "neovim/nvim-lspconfig" }, -- Required
-      {                            -- Optional
+      {                         -- Optional
         "williamboman/mason.nvim",
         build = function()
           pcall(vim.cmd, "MasonUpdate")
@@ -13,14 +13,14 @@ return {
       },
       { "williamboman/mason-lspconfig.nvim" }, -- Optional
       -- Autocompletion
-      { "hrsh7th/nvim-cmp" },                  -- Required
-      { "hrsh7th/cmp-nvim-lsp" },              -- Required
-      { "hrsh7th/cmp-buffer" },                -- Optional
-      { "hrsh7th/cmp-path" },                  -- Optional
-      { "saadparwaiz1/cmp_luasnip" },          -- Optional
-      { "hrsh7th/cmp-nvim-lua" },              -- Optional
+      { "hrsh7th/nvim-cmp" },               -- Required
+      { "hrsh7th/cmp-nvim-lsp" },           -- Required
+      { "hrsh7th/cmp-buffer" },             -- Optional
+      { "hrsh7th/cmp-path" },               -- Optional
+      { "saadparwaiz1/cmp_luasnip" },       -- Optional
+      { "hrsh7th/cmp-nvim-lua" },           -- Optional
       -- Signature Help
-      { "folke/neodev.nvim", opts = {} },       -- Optional
+      { "folke/neodev.nvim",                opts = {} }, -- Optional
       -- Snippets
       {
         "L3MON4D3/LuaSnip",
@@ -67,19 +67,68 @@ return {
           { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
         },
       },
-      { 'quangnguyen30192/cmp-nvim-ultisnips' },
+      { "quangnguyen30192/cmp-nvim-ultisnips" },
+      {
+        "SmiteshP/nvim-navic",
+        dependencies = { "neovim/nvim-lspconfig" },
+        config = function()
+          local icons = require("mdupree.icons")
+          require("nvim-navic").setup({
+            icons = icons.kinds,
+          })
+
+          vim.api.nvim_create_augroup("LspAttach_navic", {})
+          vim.api.nvim_create_autocmd("LspAttach", {
+            group = "LspAttach_navic",
+            callback = function(args)
+              if not (args.data and args.data.client_id) then
+                return
+              end
+
+              local bufnr = args.buf
+              local client = vim.lsp.get_client_by_id(args.data.client_id)
+              if client.server_capabilities.documentSymbolProvider then
+                require("nvim-navic").attach(client, bufnr)
+              end
+            end,
+          })
+        end,
+      },
     },
     config = function()
       --
       -- config
       --
       local lsp = require("lsp-zero")
+      local navic = require("nvim-navic")
+
       vim.opt.signcolumn = "yes"
+      local function status_line()
+        local mode = "%-5{%v:lua.string.upper(v:lua.vim.fn.mode())%}"
+        local file_name = "%-.16t"
+        local buf_nr = "[%n]"
+        local modified = " %-m"
+        local file_type = " %y"
+        local right_align = "%="
+        local line_no = "%10([%l/%L%)]"
+        local pct_thru_file = "%5p%%"
+
+        return string.format(
+          "%s%s%s%s%s%s%s",
+          file_name,
+          buf_nr,
+          modified,
+          file_type,
+          right_align,
+          line_no,
+          pct_thru_file
+        )
+      end
+
+      -- vim.opt.statusline = status_line()
+      vim.opt.winbar = status_line()
+
       lsp.preset("recommended")
-      lsp.on_attach(function(client, bufnr)
-        lsp.default_keymaps({ buffer = bufnr })
-        lsp.buffer_autoformat()
-      end)
 
       -- NOTE: Not sure if i need this with ^ buffer_autoformat
       lsp.format_on_save({
@@ -88,59 +137,58 @@ return {
           timeout_ms = 10000,
         },
         servers = {
-          ['tsserver'] = { 'javascript', 'typescript' },
-          ['rust_analyzer'] = { 'rust' },
-        }
+          ["tsserver"] = { "javascript", "typescript" },
+          ["rust_analyzer"] = { "rust" },
+        },
       })
 
       lsp.set_sign_icons({
-        error = '✘',
-        warn = '▲',
-        hint = '⚑',
-        info = '»'
+        error = "✘",
+        warn = "▲",
+        hint = "⚑",
+        info = "»",
       })
 
       --
       -- MASON Configuration
       --
-      require('mason').setup({})
-      require('mason-lspconfig').setup({
-        ensure_installed = { 'lua_ls', 'tsserver', 'rust_analyzer', 'yamlls', 'tailwindcss', 'jsonls' },
+      require("mason").setup({})
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver", "rust_analyzer", "yamlls", "tailwindcss", "jsonls" },
         handlers = {
           lsp.default_setup,
           lua_ls = function()
             local lua_opts = lsp.nvim_lua_ls()
-            require('lspconfig').lua_ls.setup(lua_opts)
+            require("lspconfig").lua_ls.setup(lua_opts)
           end,
         },
       })
-
 
       --
       -- CMP Configuration
       --
       local cmp = require("cmp")
-      local cmp_format = require('lsp-zero').cmp_format()
+      local cmp_format = require("lsp-zero").cmp_format()
       cmp.setup({
         sources = {
-          { name = 'nvim_lsp' },
-          { name = 'ultisnips' },
-          { name = 'buffer' },
+          { name = "nvim_lsp" },
+          { name = "ultisnips" },
+          { name = "buffer" },
         },
         --- (Optional) Show source name in completion menu
         --formatting = cmp_format,
         formatting = {
           -- changing the order of fields so the icon is the first
-          fields = { 'menu', 'abbr', 'kind' },
+          fields = { "menu", "abbr", "kind" },
 
           -- here is where the change happens
           format = function(entry, item)
             local menu_icon = {
-              nvim_lsp = '🛠',
-              luasnip = '🔨',
-              buffer = '🗄',
-              path = '🖫',
-              nvim_lua = 'Π',
+              nvim_lsp = "🛠",
+              luasnip = "🔨",
+              buffer = "🗄",
+              path = "🖫",
+              nvim_lua = "Π",
             }
 
             item.menu = menu_icon[entry.source.name]
@@ -148,20 +196,20 @@ return {
           end,
         },
         mapping = {
-          ['<C-y>'] = cmp.mapping.confirm({ select = false }),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<Up>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
-          ['<Down>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
-          ['<C-p>'] = cmp.mapping(function()
+          ["<C-y>"] = cmp.mapping.confirm({ select = false }),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<Up>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
+          ["<Down>"] = cmp.mapping.select_next_item({ behavior = "select" }),
+          ["<C-p>"] = cmp.mapping(function()
             if cmp.visible() then
-              cmp.select_prev_item({ behavior = 'insert' })
+              cmp.select_prev_item({ behavior = "insert" })
             else
               cmp.complete()
             end
           end),
-          ['<C-n>'] = cmp.mapping(function()
+          ["<C-n>"] = cmp.mapping(function()
             if cmp.visible() then
-              cmp.select_next_item({ behavior = 'insert' })
+              cmp.select_next_item({ behavior = "insert" })
             else
               cmp.complete()
             end
@@ -169,24 +217,47 @@ return {
         },
         snippet = {
           expand = function(args)
-            require('luasnip').lsp_expand(args.body)
+            require("luasnip").lsp_expand(args.body)
           end,
         },
       })
 
       lsp.on_attach(function(client, bufnr)
+        lsp.default_keymaps({ buffer = bufnr })
+        lsp.buffer_autoformat()
+
         local opts = { buffer = bufnr, remap = false }
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = bufnr })
+        vim.keymap.set("n", "gd", function()
+          vim.lsp.buf.definition()
+        end, opts)
+        vim.keymap.set("n", "K", function()
+          vim.lsp.buf.hover()
+        end, opts)
+        vim.keymap.set("n", "<leader>vws", function()
+          vim.lsp.buf.workspace_symbol()
+        end, opts)
+        vim.keymap.set("n", "<leader>vd", function()
+          vim.diagnostic.open_float()
+        end, opts)
+        vim.keymap.set("n", "[d", function()
+          vim.diagnostic.goto_next()
+        end, opts)
+        vim.keymap.set("n", "]d", function()
+          vim.diagnostic.goto_prev()
+        end, opts)
+        vim.keymap.set("n", "<leader>vca", function()
+          vim.lsp.buf.code_action()
+        end, opts)
+        vim.keymap.set("n", "<leader>vrr", function()
+          vim.lsp.buf.references()
+        end, opts)
+        vim.keymap.set("n", "<leader>vrn", function()
+          vim.lsp.buf.rename()
+        end, opts)
+        vim.keymap.set("i", "<C-h>", function()
+          vim.lsp.buf.signature_help()
+        end, opts)
+        vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", { buffer = bufnr })
       end)
 
       lsp.setup()
