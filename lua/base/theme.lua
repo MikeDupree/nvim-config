7b10d5aeb3b79f07d40ffd59053e4440b4ceef3e
mdupree <mdupree@acrocommerce.com>
2025-04-12 23:19:25 -0700

    update theme config for sonokai

--- lua/base/theme.lua
+++ lua/base/theme.lua
@@ -29,6 +29,11 @@ local sonokai = {
     vim.g.sonokai_style = 'andromeda'
     vim.g.sonokai_better_performance = 1
     vim.g.sonokai_transparent_background = true
+    vim.g.sonokai_cursor = 'purple'
+    vim.g.sonokai_dim_inactive_windows = true
+    vim.g.sonokai_diagnostic_line_highlight = true
+    vim.g.sonokai_diagnostic_virtual_text = 'colored'
+    --     Available values:   `'grey'`, `'colored'`, `'highlighted'`
     vim.cmd.colorscheme 'sonokai'
     require('lualine').setup {
       options = {
