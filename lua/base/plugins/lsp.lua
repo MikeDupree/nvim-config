e8b0b6af278801ed15345782a92165aacc389d68
mdupree <mdupree@acrocommerce.com>
2025-04-12 23:19:58 -0700

    lsp / syntax config updates

--- lua/base/plugins/lsp.lua
+++ lua/base/plugins/lsp.lua
@@ -210,7 +210,9 @@ return {
       --    https://github.com/pmizio/typescript-tools.nvim
       --
       -- But for many setups, the LSP (`ts_ls`) will work just fine
-      -- ts_ls = {},
+      ts_ls = {},
+      tailwindcss = {},
+      rust_analyzer = {},
       --
 
       lua_ls = {
@@ -245,6 +247,8 @@ return {
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
+      'eslint',
+      'prettier',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
