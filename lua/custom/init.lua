b03afc1419b5f4c2d018301edcac08f60746cfa0
mdupree <mdupree@acrocommerce.com>
2025-04-16 21:23:11 -0700

    Adds markdown preview and commands PeekOpen, PeekClose to open/close the preview

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,4 +1,4 @@
---- Enable the custom modules you want here
+---fEnable the custom modules you want here
 local custom = {
   require 'custom.plugins.mini',
   require 'custom.plugins.diagnostics',
@@ -8,6 +8,7 @@ local custom = {
   require 'custom.plugins.refactoring',
   require 'custom.plugins.terminal',
   require 'custom.plugins.status_line',
+  require 'custom.plugins.markdown_preview',
 }
 
 return custom
