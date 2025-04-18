3e215ea0e341fa9d4d7d511c5f9d895fa96b7716
mdupree <mdupree@acrocommerce.com>
2025-04-18 08:53:58 -0700

    Removes nvim-notify

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -3,7 +3,13 @@ return {
   event = 'VeryLazy',
   dependencies = {
     'MunifTanjim/nui.nvim',
-    'rcarriga/nvim-notify',
+    -- {
+    --   'rcarriga/nvim-notify',
+    --   opts = {
+    --     render = 'compact',
+    --   },
+    --   end,
+    -- },
   },
   config = function()
     require('noice').setup {
@@ -17,8 +23,8 @@ return {
       },
       -- you can enable a preset for easier configuration
       presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
+        bottom_search = false, -- use a classic bottom cmdline for search
+        command_palette = false, -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = false, -- add a border to hover docs and signature help
